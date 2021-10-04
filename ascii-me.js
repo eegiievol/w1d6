ce4b4f26ab56dde5f73301a77ecb8e6de91223b2
unknown <eulziisaikhan@miu.edu>
2021-10-04 10:42:46 -0500

    a3r23r2

--- ascii-me.js
+++ ascii-me.js
@@ -1,66 +1,73 @@
+/*
+
+Bilguun Batnasan
+Following script contains functionalities of ascii animator page
+
+*/
+
 window.onload = function() {
   "use strict";
 
-  let boxField = "";
-  let selectedAnimation = ANIMATIONS["Blank"];
-  let animationIdx;
-  let timer;
-  let running = false;
-  let refreshRate = 250;
+  const sizes = {
+    Tiny: "8pt",
+    Small: "10pt",
+    Medium: "12pt",
+    Large: "14pt",
+    "Extra Large": "16pt",
+    XXL: "18pt",
+  };
 
   const textArea = document.getElementById("text-area");
   const startBtn = document.getElementById("start");
   const stopBtn = document.getElementById("stop");
   const animation = document.getElementById("animation");
-  const sizes = document.getElementById("fontsize");
+  const size = document.getElementById("fontsize");
   const turbo = document.getElementById("turbo");
 
+  textArea.style.fontSize = sizes.Medium;
+
+  let currentAnim = ANIMATIONS["Blank"];
+  let speed = 250;
+  let animInd = 0;
+  let animInterval;
+  let previousText = "";
+
+  function animate() {
+    if (currentAnim.length > 0) {
+      textArea.value = currentAnim[animInd++];
+      animInd %= currentAnim.length;
+    }
+  }
+
   startBtn.onclick = function() {
-    boxField = textArea.value;
+    previousText = textArea.value;
     stopBtn.disabled = false;
     startBtn.disabled = true;
-    if (selectedAnimation) {
-      textArea.value = selectedAnimation[(animationIdx = 0)];
-      running = true;
-      timer = setTimeout(updateAnimation, refreshRate);
+    if (currentAnim) {
+      animInd = 0;
+      animInterval = setInterval(animate, speed);
     } else textArea.value = "";
   };
 
   stopBtn.onclick = function() {
-    clearTimeout();
-    running = false;
     stopBtn.disabled = true;
     startBtn.disabled = false;
-    textArea.value = boxField;
+    clearInterval(animInterval);
+    textArea.value = previousText;
   };
 
   animation.onchange = function() {
-    selectedAnimation = ANIMATIONS[animation.value].split("=====\n");
+    currentAnim = ANIMATIONS[animation.value].split("=====\n");
   };
 
-  sizes.onchange = function() {
-    const sizes = {
-      Tiny: "8pt",
-      Small: "10pt",
-      Medium: "12pt",
-      Large: "14pt",
-      "Extra Large": "16pt",
-      XXL: "18pt",
-    };
-
-    if (textArea.style.fontSize !== sizes[sizes.value])
-      textArea.style.fontSize = sizes[sizes.value];
+  size.onchange = function() {
+    if (textArea.style.fontSize !== sizes[size.value])
+      textArea.style.fontSize = sizes[size.value];
   };
 
   turbo.onchange = function() {
-    refreshRate = turbo.checked ? 50 : 250;
+    speed = speed == 250 ? 50 : 250;
+    clearInterval(animInterval);
+    animInterval = setInterval(animate, speed);
   };
-
-  function updateAnimation() {
-    if (running) {
-      textArea.value = selectedAnimation[animationIdx++];
-      animationIdx %= selectedAnimation.length;
-      setTimeout(updateAnimation, refreshRate);
-    }
-  }
 };
